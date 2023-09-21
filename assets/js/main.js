87a17278ff93f68d1ccc6a5bd0b6274cc9d07b3a
metzys <contact@metzys.net>
2023-09-22 01:25:51 +0200

    section biographie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 let btnMenu = document.querySelector('.menu-btn');
 let sidebar = document.querySelector('.sidebar');
 let headerBg = document.querySelector('.header-background');
+let navListItem = document.querySelectorAll('.nav-list__item');
 // récupération de la taille du bg pour ajuster la taille de la sidebar
 let headerBgSize = headerBg.offsetHeight;
 let height;
-// 
 
 let btnClose = document.querySelector('.btn-close');
+
+let btnContact = document.querySelector('.btn-contact');
 
 window.addEventListener('load', () => {
     height = headerBgSize + "px";
@@ -30,3 +32,13 @@ btnClose.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar-visible');
 })
 
+for (let i = 0; i < navListItem.length; i++) {
+    navListItem[i].addEventListener('click', () => {
+        sidebar.classList.toggle('hidden');
+        sidebar.classList.toggle('sidebar-visible');
+    })
+}
+
+// Template de la modale de contact
+
+let contactModal = ``;
